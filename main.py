6a37dfa267f27e0b903527c6b68d037e1391a856
itsmevosh <jaxvoscagata195@gmail.com>
2024-05-28 17:43:10 +0800

    new changes

---
+++ main.py
@@ -0,0 +1,8 @@
+vosh = int(input("how many vosh?:"))
+columns = int(input("how many vosh?: "))
+symbol  = input("Enter a symbol to use: ")
+
+for i in range(vosh):
+ for j in range(columns):
+   print(symbol, end="")
+ print()  
