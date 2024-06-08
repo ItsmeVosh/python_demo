ae738cdd558a29f57a578bb1a2d6da3186a0bad0
itsmevosh <jaxvoscagata195@gmail.com>
2024-06-09 05:39:29 +0800

    feat:nested-loops

---
+++ nested.py
@@ -0,0 +1,9 @@
+rose =int(input("how many rose: "))
+columns =int(input("how many rose: "))
+symbol =input("Enter a symbol to use: ")
+
+for i in range(rose):
+    for j in range(columns):
+        print(symbol, end="")
+    print()
+
--- vosh.py
+++
@@ -1,2 +0,0 @@
-names = "vosh cagata"
-print(names)
