ec9abf979cdeb05978908571eb12cfaf36cd8051
itsmevosh <jaxvoscagata195@gmail.com>
2024-06-08 08:32:21 +0800

    feat: delete garbage

--- main.py
+++
@@ -1,8 +0,0 @@
-vosh = int(input("how many vosh?:"))
-columns = int(input("how many vosh?: "))
-symbol  = input("Enter a symbol to use: ")
-
-for i in range(vosh):
- for j in range(columns):
-   print(symbol, end="")
- print()  
